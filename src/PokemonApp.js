e94c79b940eb100bf529a25f0ac9fc639f18bbf6
mirhamasala <mirha.masala@me.com>
2021-03-01 11:37:34 +0800

    fetch Pokemon from API

--- src/PokemonApp.js
+++ src/PokemonApp.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './PokemonApp.scss';
 
 import PokemonForm from './components/PokemonForm';
 
 function PokemonApp() {
+  const [status, setStatus] = useState('idle');
+  const [pokemon, setPokemon] = useState(null);
   const [pokemonName, setPokemonName] = useState(null);
+
+  useEffect(() => {
+    if (!pokemonName) {
+      return;
+    }
+    setStatus('pending');
+    getPokemon(pokemonName);
+  }, [pokemonName]);
+
+  const getPokemon = (query) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+      .then((response) => response.json())
+      .then((pokemonData) => {
+        setStatus('resolved');
+        setPokemon(pokemonData);
+      });
+  };
 
   const handleToUpdate = (pokemonName) => setPokemonName(pokemonName);
 
   return (
     <div className="PokemonApp">
       <PokemonForm handleToUpdate={handleToUpdate} />
-      {pokemonName}
+      {status === 'resolved' && pokemon && <div>{pokemon.name}</div>}
     </div>
   );
 }
